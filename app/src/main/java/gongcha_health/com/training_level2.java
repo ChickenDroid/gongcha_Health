cf74966cd9b7180448bcbd4d1d88058400e0371d
김준성 <anima94@naver.com>
2020-03-03 18:49:54 +0900

    feat) set up Viewpager and indicator (Image is sample must change)

---
+++ app/src/main/java/gongcha_health/com/training_level2.java
@@ -0,0 +1,26 @@
+package gongcha_health.com;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.fragment.app.Fragment;
+
+public class training_level2 extends Fragment {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.training_level2, container, false);
+
+
+        return view;
+    }
+}
